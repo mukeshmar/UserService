53d08bedb31e8c91a09e1454d42175371ec971f4
Mukesh Mar <mukeshmar0710@gmail.com>
2024-09-28 15:42:32 +0530

    feat: Add UserService class with method signatures

--- src/main/java/live/mukeshtechlab/userservice/services/UserService.java
+++ src/main/java/live/mukeshtechlab/userservice/services/UserService.java
@@ -1,4 +1,24 @@
 package live.mukeshtechlab.userservice.services;
 
+import live.mukeshtechlab.userservice.models.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private User singUp(String name, String email, String password){
+        return null;
+    }
+
+    private User login(String email, String password){
+        return null;
+    }
+
+    private User validateToken(String token){
+        return null;
+    }
+
+    private void logout(String token){
+
+    }
 }
